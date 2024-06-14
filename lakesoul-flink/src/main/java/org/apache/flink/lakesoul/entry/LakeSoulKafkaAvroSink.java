b10dc5ac224c140f15170707e5e5b0a0277bb902
fphantam <dongf@dmetasoul.com>
2024-06-14 19:48:46 +0800

    improve data from lakesoul to kafka avro
    Signed-off-by: fphantam <dongf@dmetasoul.com>

--- lakesoul-flink/src/main/java/org/apache/flink/lakesoul/entry/LakeSoulKafkaAvroSink.java
+++ lakesoul-flink/src/main/java/org/apache/flink/lakesoul/entry/LakeSoulKafkaAvroSink.java
@@ -1,49 +1,83 @@
+// SPDX-FileCopyrightText: 2023 LakeSoul Contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.apache.flink.lakesoul.entry;
 
-import com.google.common.collect.Maps;
+import com.dmetasoul.lakesoul.meta.DBManager;
+import com.dmetasoul.lakesoul.meta.DBUtil;
+import com.dmetasoul.lakesoul.meta.entity.TableInfo;
+import org.apache.arrow.vector.types.pojo.Schema;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.formats.avro.RowDataToAvroConverters;
+import org.apache.flink.formats.avro.RowDataToAvroConverters.RowDataToAvroConverter;
+import org.apache.flink.formats.avro.registry.confluent.ConfluentRegistryAvroSerializationSchema;
+import org.apache.flink.formats.avro.typeutils.AvroSchemaConverter;
+import org.apache.flink.lakesoul.source.arrow.LakeSoulArrowSource;
+import org.apache.flink.lakesoul.tool.LakeSoulKeyGen;
 import org.apache.flink.lakesoul.tool.LakeSoulSinkOptions;
 import org.apache.flink.lakesoul.types.*;
 import org.apache.flink.lakesoul.types.arrow.LakeSoulArrowWrapper;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.ExecutionCheckpointingOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.data.RowData.FieldGetter;
+import org.apache.flink.table.data.StringData;
 import org.apache.flink.table.runtime.arrow.ArrowReader;
 import org.apache.flink.table.runtime.arrow.ArrowUtils;
+import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.VarCharType;
+import org.apache.flink.types.RowKind;
 import org.apache.flink.util.Collector;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.apache.flink.lakesoul.tool.JobOptions.*;
 import static org.apache.flink.lakesoul.tool.LakeSoulKafkaSinkOptions.*;
 import static org.apache.flink.lakesoul.tool.LakeSoulSinkOptions.SERVER_TIME_ZONE;
 
-public class LakeSoulKafkaAvroSink {
+public class LakeSoulKafkaAvroSink_1 {
     public static void main(String[] args) throws Exception {
         ParameterTool parameter = ParameterTool.fromArgs(args);
         String kafkaServers = parameter.get(BOOTSTRAP_SERVERS.key());
         String kafkaTopic = parameter.get(TOPIC.key());
         String topicGroupID = parameter.get(GROUP_ID.key());
 
+        String lakeSoulDBName = parameter.get(DBNAME.key());
+        String lakeSoulTableName = parameter.get(TABLE_NAME.key());
+
         int sourceParallelism = parameter.getInt(SOURCE_PARALLELISM.key(), 1);
         int sinkParallelism = parameter.getInt(BUCKET_PARALLELISM.key(), 1);
-        int checkpointInterval = parameter.getInt(JOB_CHECKPOINT_INTERVAL.key(), JOB_CHECKPOINT_INTERVAL.defaultValue());
+        int checkpointInterval = parameter.getInt(JOB_CHECKPOINT_INTERVAL.key(),
+                JOB_CHECKPOINT_INTERVAL.defaultValue());
         String serverTimezone = parameter.get(SERVER_TIME_ZONE.key(), SERVER_TIME_ZONE.defaultValue());
         String schemaRegistryUrl = parameter.get(SCHEMA_REGISTRY_URL.key(), SCHEMA_REGISTRY_URL.defaultValue());
-        boolean regularTopicName = parameter.getBoolean(REGULAR_TOPIC_NAME.key(), REGULAR_TOPIC_NAME.defaultValue());
         int maxPollRecords = parameter.getInt(MAX_POLL_RECORDS.key(), MAX_POLL_RECORDS.defaultValue());
 
         //about security
@@ -60,6 +94,7 @@ public class LakeSoulKafkaAvroSink {
         pro.put("group.id", topicGroupID);
         pro.put("max.poll.records", maxPollRecords);
         pro.put("retries", 3);
+        pro.put("transaction.timeout.ms", "60000");
 
         OffsetsInitializer offSet;
         if (securityProtocol != null) {
@@ -100,8 +135,9 @@ public class LakeSoulKafkaAvroSink {
         conf.set(LakeSoulSinkOptions.BUCKET_PARALLELISM, sinkParallelism);
         conf.set(ExecutionCheckpointingOptions.ENABLE_CHECKPOINTS_AFTER_TASKS_FINISH, true);
 
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
-        env.getConfig().registerTypeWithKryoSerializer(BinarySourceRecord.class, BinarySourceRecordSerializer.class);
+        // StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        conf.set(RestOptions.PORT, 8181);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
         ParameterTool pt = ParameterTool.fromMap(conf.toMap());
         env.getConfig().setGlobalJobParameters(pt);
         env.enableCheckpointing(checkpointInterval);
@@ -113,7 +149,8 @@ public class LakeSoulKafkaAvroSink {
         }
         env.getCheckpointConfig().setTolerableCheckpointFailureNumber(5);
         env.getCheckpointConfig().setCheckpointingMode(checkpointingMode);
-        env.getCheckpointConfig().setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        env.getCheckpointConfig().setExternalizedCheckpointCleanup(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
         env.getCheckpointConfig().setCheckpointStorage(parameter.get(FLINK_CHECKPOINT.key()));
         env.setRestartStrategy(RestartStrategies.failureRateRestart(
@@ -124,48 +161,194 @@ public class LakeSoulKafkaAvroSink {
 
         DataStreamSource<LakeSoulArrowWrapper> source = env.fromSource(
                 LakeSoulArrowSource.create(
-                        "default",
-                        MockLakeSoulArrowSource.MockSourceFunction.tableName,
+                        lakeSoulDBName,
+                        lakeSoulTableName,
                         conf
                 ),
                 WatermarkStrategy.noWatermarks(),
                 "LakeSoul Arrow Source"
         );
 
-        LakeSoulDynamicKafkaRecordSerializationSchema lakeSoulDynamicKafkaRecordSerializationSchema =
-                LakeSoulDynamicKafkaRecordSerializationSchema.builder()
-                        .setTopic(kafkaTopic)
-                        .setSchemaRegistryUrl(schemaRegistryUrl)
-                        .setKafkaConfigs(Maps.fromProperties(pro))
-                        .setLakeDBAndTable("", "")
-                        .hsaMetaData(false)
-                        .setMetadataPositions(null)
-                        .isUpsertMode(false)
-                        .build();
+        Map<String, Object> props = new HashMap<String, Object>((Map) pro);
 
-        KafkaSink<SinkKafkaRecord> kafkaSink = KafkaSink.<SinkKafkaRecord>builder()
+        KafkaSink<Tuple3<byte[], byte[], Long>> kafkaSink = KafkaSink.<Tuple3<byte[], byte[], Long>>builder()
                 .setBootstrapServers(kafkaServers)
-                .setRecordSerializer(lakeSoulDynamicKafkaRecordSerializationSchema)
+                .setRecordSerializer(new LakeSoulDynamicKafkaRecordSerializationSchema(kafkaTopic, new LakSoulKafkaPartitioner<>()))
                 .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .setKafkaProducerConfig(pro)
                 .build();
 
-        source.flatMap(new FlatMapFunction<LakeSoulArrowWrapper, SinkKafkaRecord>() {
-            @Override
-            public void flatMap(LakeSoulArrowWrapper lakeSoulArrowWrapper, Collector<SinkKafkaRecord> collector) throws Exception {
-                TableSchemaIdentity tableSchemaIdentity = lakeSoulArrowWrapper.generateTableSchemaIdentity();
-                RowType rowType = tableSchemaIdentity.rowType;
-                lakeSoulArrowWrapper.withDecoded(ArrowUtils.getRootAllocator(), (tableInfo, recordBatch) -> {
-                    ArrowReader arrowReader = ArrowUtils.createArrowReader(recordBatch, rowType);
-                    int i = 0;
-                    while (i < recordBatch.getRowCount()) {
-                        RowData rowData = arrowReader.read(i);
-                        SinkKafkaRecord sinkKafkaRecord = new SinkKafkaRecord(tableSchemaIdentity, rowData, rowType);
-                        collector.collect(sinkKafkaRecord);
-                        i++;
+
+        Tuple4<ConfluentRegistryAvroSerializationSchema, RowDataToAvroConverter, RowType, RowData.FieldGetter[]>  keyInfo = getKeyInfo(lakeSoulDBName,
+                lakeSoulTableName, kafkaTopic, schemaRegistryUrl, props);
+        ConfluentRegistryAvroSerializationSchema keySerialization;
+        RowDataToAvroConverter keyRowDataToAvroConverter;
+        final RowType keyRowType;
+        FieldGetter[] keyFieldGetters;
+        if (keyInfo != null) {
+            keySerialization = keyInfo.f0;
+            keyRowDataToAvroConverter = keyInfo.f1 ;
+            keyRowType = keyInfo.f2;
+            keyFieldGetters = keyInfo.f3;
+        } else {
+            keySerialization = null;
+            keyRowDataToAvroConverter = null;
+            keyRowType = null;
+            keyFieldGetters = null;
+        }
+
+
+        SingleOutputStreamOperator<Tuple3<byte[], byte[], Long>> sinkKafkaRecordSingleOutputStreamOperator = source.flatMap(
+                new FlatMapFunction<LakeSoulArrowWrapper, Tuple3<byte[], byte[], Long>>() {
+                    @Override
+                    public void flatMap(LakeSoulArrowWrapper lakeSoulArrowWrapper, Collector<Tuple3<byte[], byte[], Long>> collector)
+                            throws Exception {
+                        TableSchemaIdentity tableSchemaIdentity = lakeSoulArrowWrapper.generateTableSchemaIdentity();
+                        RowType rowType = tableSchemaIdentity.rowType;
+                        boolean useCDC = tableSchemaIdentity.useCDC;
+                        String cdcColumn = tableSchemaIdentity.cdcColumn;
+                        RowType kafkaAvroType = toKafkaAvroType(rowType, useCDC, cdcColumn);
+
+                        lakeSoulArrowWrapper.withDecoded(ArrowUtils.getRootAllocator(), (tableInfo, recordBatch) -> {
+                            ArrowReader arrowReader = ArrowUtils.createArrowReader(recordBatch, rowType);
+                            int i = 0;
+                            System.out.println(recordBatch.getRowCount());
+                            while (i < recordBatch.getRowCount()) {
+                                System.out.println(i);
+                                RowData rowData = arrowReader.read(i);
+                                RowData kafkaRowData = toKafkaAvroRawData(rowData, rowType, String.format("%s.%s", lakeSoulDBName, lakeSoulTableName));
+
+                                byte[] keyBytes = null;
+                                if (keySerialization != null ) {
+                                    GenericRecord keyGenericRecord = (GenericRecord) keyRowDataToAvroConverter.convert(
+                                            AvroSchemaConverter.convertToSchema(keyRowType),
+                                            createProjectedRow(rowData, RowKind.INSERT, keyFieldGetters));
+                                    keyBytes = keySerialization.serialize(keyGenericRecord);
+                                }
+
+                                ConfluentRegistryAvroSerializationSchema<GenericRecord> genericRecordConfluentRegistryAvroSerializationSchema =
+                                        ConfluentRegistryAvroSerializationSchema.forGeneric(
+                                                String.format("%s-value", kafkaTopic),
+                                                AvroSchemaConverter.convertToSchema(kafkaAvroType),
+                                                schemaRegistryUrl,
+                                                props);
+                                RowDataToAvroConverter valueConverter = RowDataToAvroConverters.createConverter(kafkaAvroType);
+                                GenericRecord valueGenericRecord = (GenericRecord) valueConverter.convert(AvroSchemaConverter.convertToSchema(kafkaAvroType), kafkaRowData);
+                                System.out.println(valueGenericRecord.toString());
+                                byte[] valueBytes = genericRecordConfluentRegistryAvroSerializationSchema.serialize(valueGenericRecord);
+
+                                long hash = 42;
+                                List<String> primaryKeys = tableSchemaIdentity.primaryKeys;
+                                for (String pk : primaryKeys) {
+                                    int typeIndex = rowType.getFieldIndex(pk);
+                                    LogicalType type = rowType.getTypeAt(typeIndex);
+                                    Object fieldOrNull = RowData.createFieldGetter(type, typeIndex)
+                                            .getFieldOrNull(rowData);
+                                    hash = LakeSoulKeyGen.getHash(type, fieldOrNull, hash);
+                                }
+
+                                collector.collect(new Tuple3<>(keyBytes, valueBytes, hash));
+                                i++;
+                            }
+                        });
                     }
                 });
-            }
-        }).sinkTo(kafkaSink);
+
+        // sinkKafkaRecordSingleOutputStreamOperator.print();
+        sinkKafkaRecordSingleOutputStreamOperator.sinkTo(kafkaSink);
         env.execute("LakeSoul CDC Sink From Kafka topic " + kafkaTopic);
+    }
+
+    public static Tuple4<ConfluentRegistryAvroSerializationSchema, RowDataToAvroConverter, RowType, RowData.FieldGetter[]> getKeyInfo(String dbName,
+                                                                                                                                      String table, String topic, String schemaRegistryUrl, Map<String, ?> kafkaConfigs) {
+        DBManager lakesoulDBManager = new DBManager();
+        TableInfo tableInfo = lakesoulDBManager.getTableInfoByNameAndNamespace(table, dbName);
+        String tableSchema = tableInfo.getTableSchema();
+        DBUtil.TablePartitionKeys partitionKeys = DBUtil.parseTableInfoPartitions(tableInfo.getPartitions());
+        List<String> primaryKeys = partitionKeys.primaryKeys;
+
+        final RowData.FieldGetter[] keyFieldGetters;
+        RowType keyRowType;
+        int[] keyIndex;;
+
+        if (primaryKeys.size() > 0) {
+            try {
+                Schema schema = Schema.fromJSON(tableSchema);
+                List<String> columns = schema.getFields().stream().map(f -> f.getName()).collect(Collectors.toList());
+                keyIndex = Arrays.stream(primaryKeys.toArray(new String[0])).mapToInt(columns::indexOf).toArray();
+                keyRowType = ArrowUtils.tablePrimaryArrowSchema(schema, primaryKeys);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            keyFieldGetters =
+                    IntStream.range(0, keyRowType.getFieldCount())
+                            .mapToObj(
+                                    i ->
+                                            RowData.createFieldGetter(
+                                                    keyRowType.getTypeAt(i), keyIndex[i]))
+                            .toArray(RowData.FieldGetter[]::new);
+
+            ConfluentRegistryAvroSerializationSchema<GenericRecord>
+                    genericRecordConfluentRegistryAvroSerializationSchema =
+                    ConfluentRegistryAvroSerializationSchema.forGeneric(
+                            String.format("%s-key", topic),
+                            AvroSchemaConverter.convertToSchema(keyRowType),
+                            schemaRegistryUrl,
+                            kafkaConfigs);
+            RowDataToAvroConverter keyRowDataToAvroConverter = RowDataToAvroConverters.createConverter(keyRowType);
+            return new Tuple4<>(genericRecordConfluentRegistryAvroSerializationSchema, keyRowDataToAvroConverter, keyRowType, keyFieldGetters);
+        }
+        return null;
+    }
+
+    public static RowData createProjectedRow(
+            RowData consumedRow, RowKind kind, RowData.FieldGetter[] fieldGetters) {
+        final int arity = fieldGetters.length;
+        final GenericRowData genericRowData = new GenericRowData(kind, arity);
+        for (int fieldPos = 0; fieldPos < arity; fieldPos++) {
+            genericRowData.setField(fieldPos, fieldGetters[fieldPos].getFieldOrNull(consumedRow));
+        }
+        return genericRowData;
+    }
+
+    public static RowType toKafkaAvroType(RowType rowType, boolean useCDC, String cdcColumn) {
+        LogicalType[] colTypes = new LogicalType[rowType.getFieldCount() + 2];
+        String[] colNames = new String[rowType.getFieldCount() + 2];
+        for (int i = 0; i < rowType.getFieldCount(); ++i) {
+            String colName = rowType.getFieldNames().get(i);
+            if (useCDC && colName.equals(cdcColumn)) {
+                colNames[i] = "op_type";
+            } else {
+                colNames[i] = colName;
+            }
+            colTypes[i] = rowType.getTypeAt(i);
+        }
+        colNames[rowType.getFieldCount()] = "table";
+        colTypes[rowType.getFieldCount()] = new VarCharType(false, Integer.MAX_VALUE);
+        colNames[rowType.getFieldCount() + 1] = "op_ts";
+        colTypes[rowType.getFieldCount() + 1] = new VarCharType(false, Integer.MAX_VALUE);
+        return RowType.of(colTypes, colNames);
+    }
+
+    public static RowData toKafkaAvroRawData(RowData rowData, RowType rowType, String table) {
+
+        RowData.FieldGetter[] fieldGetters =
+                IntStream.range(0, rowType.getFieldCount())
+                        .mapToObj(
+                                i ->
+                                        RowData.createFieldGetter(
+                                                rowType.getTypeAt(i), i))
+                        .toArray(RowData.FieldGetter[]::new);
+
+        int newArity = rowData.getArity() + 2;
+        GenericRowData kafkaRowData = new GenericRowData(newArity);
+        for (int i = 0; i < newArity - 2; ++i) {
+            kafkaRowData.setField(i, fieldGetters[i].getFieldOrNull(rowData));
+        }
+        kafkaRowData.setRowKind(rowData.getRowKind());
+        kafkaRowData.setField(kafkaRowData.getArity() - 2, StringData.fromString(table));
+        kafkaRowData.setField(kafkaRowData.getArity() - 1, StringData.fromString(String.valueOf(System.currentTimeMillis())));
+        return kafkaRowData;
     }
 }
--- lakesoul-flink/src/main/java/org/apache/flink/lakesoul/types/LakSoulKafkaPartitioner.java
+++ lakesoul-flink/src/main/java/org/apache/flink/lakesoul/types/LakSoulKafkaPartitioner.java
@@ -1,22 +1,12 @@
+// SPDX-FileCopyrightText: 2023 LakeSoul Contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.apache.flink.lakesoul.types;
 
-import org.apache.arrow.memory.BufferAllocator;
-import org.apache.arrow.vector.VectorSchemaRoot;
-import org.apache.arrow.vector.ipc.ArrowStreamReader;
-import org.apache.flink.lakesoul.tool.LakeSoulKeyGen;
-import org.apache.flink.lakesoul.types.arrow.LakeSoulArrowWrapper;
 import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkFixedPartitioner;
 import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkKafkaPartitioner;
-import org.apache.flink.table.data.RowData;
-import org.apache.flink.table.runtime.arrow.ArrowReader;
-import org.apache.flink.table.runtime.arrow.ArrowUtils;
-import org.apache.flink.table.types.logical.LogicalType;
-import org.apache.flink.table.types.logical.RowType;
 import org.apache.flink.util.Preconditions;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.List;
 
 public class LakSoulKafkaPartitioner<T> extends FlinkKafkaPartitioner<T> {
 
@@ -40,47 +30,11 @@ public class LakSoulKafkaPartitioner<T> extends FlinkKafkaPartitioner<T> {
                 partitions != null && partitions.length > 0,
                 "Partitions of the target topic is empty.");
 
-        if (record instanceof LakeSoulArrowWrapper) {
-
-            TableSchemaIdentity tableSchemaIdentity = ((LakeSoulArrowWrapper) record).generateTableSchemaIdentity();
-            RowType rowType = tableSchemaIdentity.rowType;
-
-            byte[] encodedBatch = ((LakeSoulArrowWrapper) record).getEncodedBatch();
-            BufferAllocator allocator = ArrowUtils.getRootAllocator().newChildAllocator("allocator", 0, Long.MAX_VALUE);
-            ArrowStreamReader arrowStreamReader = new ArrowStreamReader(new ByteArrayInputStream(encodedBatch), allocator);
-            VectorSchemaRoot vectorSchemaRoot;
-            try {
-                arrowStreamReader.loadNextBatch();
-                vectorSchemaRoot = arrowStreamReader.getVectorSchemaRoot();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            ArrowReader arrowReader = ArrowUtils.createArrowReader(vectorSchemaRoot, rowType);
-            RowData rowData = arrowReader.read(0);
-
-            long hash = 42;
-            List<String> primaryKeys = tableSchemaIdentity.primaryKeys;
-            for (String pk : primaryKeys) {
-                int typeIndex = rowType.getFieldIndex(pk);
-                LogicalType type = rowType.getTypeAt(typeIndex);
-                Object fieldOrNull = RowData.createFieldGetter(type, typeIndex).getFieldOrNull(rowData);
-                hash = LakeSoulKeyGen.getHash(type, fieldOrNull, hash);
-            }
-            return partitions[(int) (hash % partitions.length)];
-        } else if (record instanceof SinkKafkaRecord) {
-            TableSchemaIdentity tableSchemaIdentity = ((SinkKafkaRecord) record).getTableSchemaIdentity();
-            RowData rowData = ((SinkKafkaRecord) record).getRowData();
-            RowType rowType = ((SinkKafkaRecord) record).getRowType();
-            long hash = 42;
-            List<String> primaryKeys = tableSchemaIdentity.primaryKeys;
-            for (String pk : primaryKeys) {
-                int typeIndex = rowType.getFieldIndex(pk);
-                LogicalType type = rowType.getTypeAt(typeIndex);
-                Object fieldOrNull = RowData.createFieldGetter(type, typeIndex).getFieldOrNull(rowData);
-                hash = LakeSoulKeyGen.getHash(type, fieldOrNull, hash);
+        if (record == null) {
+            if (record instanceof Long) {
+                return partitions[(int) ((Long) record % partitions.length)];
             }
         }
-
         return partitions[parallelInstanceId % partitions.length];
     }
 
